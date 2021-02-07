b33fab2a7582074d30fc0bd3143a10d902080c10
dumeng <spr.du@qq.com>
2021-02-07 10:17:04 +0800

    observable

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,33 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
+    <Demo />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import Vue from 'vue'
+const state = Vue.observable({ count: 0 })
+
+const Demo = {
+  render(h) {
+    return h(
+      'button',
+      {
+        on: {
+          click: () => {
+            state.count++
+          }
+        }
+      },
+      `count is: ${state.count}`
+    )
+  }
+}
 
 export default {
-  name: "Home",
-  components: {}
-};
+  name: 'Home',
+  components: { Demo }
+}
 </script>
